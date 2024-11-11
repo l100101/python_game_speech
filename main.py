547b74ab1c6045829458e196fc1b589d018d09f5
l100101 <ltlrg779@gmail.com>
2024-11-11 20:06:43 +0400

    object_A, object_B, object_A_Box

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Название игры
-pygame.display.set_caption("Пересечение объектов")
+pygame.display.set_caption("Учусь читать")
 
 # Цвета
 white = (255, 255, 255)
@@ -18,18 +18,26 @@ red = (255, 0, 0)
 blue = (0, 0, 255)
 
 # Массивы объектов
-objects1 = []
-objects2 = []
+objects_A_Box = []
+objects_O_Box = []
+objects_A = []
+objects_O = []
+objects_Y = []
 
+# image_A_Box = pygame.image.load("alphabet/A_Box.png")
+# image_O_Box = pygame.image.load("alphabet/O_Box.png")
 image_A = pygame.image.load("alphabet/A.png")
 image_O = pygame.image.load("alphabet/O.png")
+image_Y = pygame.image.load("alphabet/Y.png")
+
 
 # Создание объектов
 for i in range(10):
     # objects1.append(pygame.Rect(50 + i * 70, 50, 50, 50))
-    objects1.append(pygame.Rect(50 + i * 70, 50, image_A.get_width(), image_A.get_height()))
-    objects2.append(pygame.Rect(50 + i * 70, 300, 50, 50))
-
+    objects_A.append(pygame.Rect(50 + i * 70, 50, image_A.get_width(), image_A.get_height()))
+    objects_O.append(pygame.Rect(50 + i * 70, 200, image_O.get_width(), image_O.get_height()))
+    objects_A_Box.append(pygame.Rect(50 + i * 70, 300, 50, 50))
+    
 # Переменная для хранения перетаскиваемого объекта
 dragging = None
 
@@ -40,7 +48,7 @@ while True:
             pygame.quit()
             quit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            for obj in objects2:
+            for obj in objects_A_Box:
                 if obj.collidepoint(event.pos):
                     dragging = obj
         elif event.type == pygame.MOUSEBUTTONUP:
@@ -53,15 +61,16 @@ while True:
     screen.fill(white)
 
     # Рисование объектов
-    for obj in objects1:
+    for obj in objects_A:
         screen.blit(image_A, obj)
-        # screen.blit(image_O, obj)
-    for obj in objects2:
+    for obj in objects_O:
+        screen.blit(image_O, obj)
+    for obj in objects_A_Box:
         pygame.draw.rect(screen, blue, obj, 5)
 
     # Проверка пересечения объектов
-    for obj1 in objects1:
-        for obj2 in objects2:
+    for obj1 in objects_A_Box:
+        for obj2 in objects_A:
             if obj1.colliderect(obj2):
                 print("Пересечение!")
 
